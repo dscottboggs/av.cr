75f0b0d3450b8d8b3e1774c80b1756159a5f3c3c
D. Scott Boggs <scott@tams.tech>
2019-03-04 13:02:27 -0500

    Added more require statements to the root av.cr file

--- src/av.cr
+++ src/av.cr
@@ -2,6 +2,8 @@ require "./bindings/libavcodec"
 require "./bindings/libavfilter"
 require "./bindings/libavformat"
 require "./bindings/libavresample"
+require "./bindings/libswresample"
+require "./bindings/libavutil/libavutil"
 
 module AV
   VERSION = "0.1.0"
